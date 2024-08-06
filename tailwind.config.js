3dbc068cd6b52362a47a7e6c853badaf659c0be8
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-06 10:42:16 +0200

    Updated tailwind.config.js to include content paths and added primary color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.(vue,js,ts,jsx,tsx)"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#fcfcfc',
+          dark: '#121425',
+          medium: '#4f2c2c',
+          accent1: '#8c4433',
+          accent2: '#c95c3b',
+        },
+      },
+    },
   },
   plugins: [],
 }
