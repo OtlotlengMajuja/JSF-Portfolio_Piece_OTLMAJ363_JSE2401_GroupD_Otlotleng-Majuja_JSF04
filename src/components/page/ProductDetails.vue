75d854ecee67c9a9e7eacec8f4f40cc3bd93070d
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-13 09:41:47 +0200

    Nested the 'Back to Products' router in a button to rectify the styling

--- src/components/page/ProductDetails.vue
+++ src/components/page/ProductDetails.vue
@@ -45,12 +45,11 @@
           >
             Add to Comparison
           </button>
-          <router-link
-            to="/products"
-            class="bg-black text-white px-4 py-2 rounded hover:bg-black transition-colors duration-300 mt-4"
+          <button
+            class="bg-black text-white px-4 py-2 rounded hover:bg-black transition-colors duration-300 mt-4 ml-2"
           >
-            Back to Products
-          </router-link>
+            <router-link to="/products"> Back to Products </router-link>
+          </button>
         </div>
       </div>
     </div>
