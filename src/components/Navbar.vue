6f374f2f2819b640834c81f8492c0c22ec6bdb85
Otlotleng Majuja <thabangmajuja@gmail.com>
2024-08-17 10:50:19 +0200

    Added theme toggle button to `Navbar`

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -132,8 +132,82 @@
           Login
         </router-link>
       </div>
+      <button
+        @click="toggleTheme"
+        class="ml-4 p-2 rounded-full hover:bg-primary-accent1"
+        :aria-label="
+          isDarkMode ? 'Switch to light mode' : 'Switch to dark mode'
+        "
+      >
+        <svg
+          v-if="isDarkMode"
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+          />
+        </svg>
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      </button>
     </div>
   </nav>
+  <button
+    @click="toggleTheme"
+    class="ml-4 p-2 rounded-full hover:bg-primary-accent1"
+    :aria-label="isDarkMode ? 'Switch to light mode' : 'Switch to dark mode'"
+  >
+    <svg
+      v-if="isDarkMode"
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+      />
+    </svg>
+    <svg
+      v-else
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+      />
+    </svg>
+  </button>
 </template>
 
 <style scoped>
@@ -190,6 +264,7 @@ export default {
       () => store.getters.comparisonListCount
     );
     const isDarkMode = computed(() => store.getters.currentTheme === "dark");
+    const wishlistItemsCount = computed(() => store.getters.wishlistItemsCount);
 
     /**
      * Toggles the mobile menu open/closed state.
